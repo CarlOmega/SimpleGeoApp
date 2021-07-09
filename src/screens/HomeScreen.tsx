27adbd820e62459da9f25ab226d6639ad1b7a166
Carl Humphries <carl.w.humphries@gmail.com>
2021-07-09 10:32:16 +1000

    feat: geolocation
    Added geolocation package to get current pos.

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import { SafeAreaView, StyleSheet, Text } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+
+import Geolocation, { GeolocationResponse } from '@react-native-community/geolocation';
 
 const HomeScreen = ({navigation, route}: any) => {
+  const [location, setLocation] = useState<GeolocationResponse | null>(null);
+
+  const loadPos = async () => {
+    Geolocation.getCurrentPosition(info => setLocation(info));
+  }
+
+  useEffect(() => {
+    loadPos();
+  }, []);
 
   return (
     <SafeAreaView style={styles.screen}>
       <Text style={styles.text}>Home Screen</Text>
-      <Icon name="rocket" size={30} color="#900" />
+      <Text>{location?.coords.latitude}</Text>
+      <Text>{location?.coords.longitude}</Text>
     </SafeAreaView>
   )
 }
